9704e910b54d0fe1cd3f948c3af978fbeb4d1aa5
Florian Dahlitz <f2dahlitz@freenet.de>
2019-11-09 11:15:27 +0100

    feat(model): create link model

---
+++ app/data/link.py
@@ -0,0 +1,17 @@
+import datetime
+
+import sqlalchemy as sa
+
+from app.data.modelbase import SqlAlchemyBase
+
+
+class Link(SqlAlchemyBase):
+    __tablename__ = "links"
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    created_date = sa.Column(sa.DateTime, default=datetime.datetime.now, index=True)
+    short_link = sa.Column(sa.String, nullable=False)
+    destination = sa.Column(sa.String, nullable=False)
+
+    def __repr__(self):
+        return '<Link {} --> {}>'.format(self.short_link, self.destination)
